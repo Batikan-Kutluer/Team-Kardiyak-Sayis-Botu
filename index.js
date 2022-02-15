3c53c2084899b3baa7524c976328a126774f51f8
Batikan <60034174+Batikan-Kutluer@users.noreply.github.com>
2022-02-15 23:48:46 +0300

    hotfix 2

--- index.js
+++ index.js
@@ -27,117 +27,121 @@ let auth = (m) =>
   !m.member.permissions.has(Discord.Permissions.FLAGS.KICK_MEMBERS);
 
 client.on("messageCreate", (m) => {
-  let args = m.cleanContent.split(" ");
+  try {
+    let args = m.cleanContent.split(" ");
 
-  switch (args[0] || m.cleanContent) {
-    case "--test":
-      if (auth(m)) return;
+    switch (args[0] || m.cleanContent) {
+      case "--test":
+        if (auth(m)) return;
 
-      m.reply("Pong!");
-      break;
-    case "--set":
-      if (auth(m)) return;
+        m.reply("Pong!");
+        break;
+      case "--set":
+        if (auth(m)) return;
 
-      if (Number(args[1])) {
-        last.num = Number(args[1]);
-        fs.writeFileSync("./db.json", JSON.stringify(last), "utf8");
-      }
+        if (Number(args[1])) {
+          last.num = Number(args[1]);
+          fs.writeFileSync("./db.json", JSON.stringify(last), "utf8");
+        }
 
-      break;
-    case "--sayi":
-      m.reply(`${last.num || "0"}`)
-        .then(() => {
-          setTimeout((_) => {
-            _.delete();
-            m.delete();
-          }, 3 * 1000);
-        })
-        .catch(() => {});
-      break;
-
-    case "--stop":
-      if (auth(m)) return;
-
-      stop = true;
-      m.reply("Stopping...").then((t) => {
-        setTimeout(() => {
-          t.delete().catch(() => {
-            m.channel.send("Yeterli iznim yok!");
-          });
-        }, 3000);
-      });
-      break;
-
-    case "--start":
-      if (auth(m)) return;
-
-      stop = false;
-      break;
-
-    case "--reset":
-      if (auth(m)) return;
-
-      last.num = 0;
-      last.user = "";
-      fs.writeFileSync("./db.json", JSON.stringify(last), "utf8");
-      break;
-
-    case "--gamechannel":
-      console.log(auth(m));
-      if (auth(m)) return;
-
-      game_channel = m.channel.id;
-      break;
-
-    default:
-      if (!Number(m.cleanContent)) {
-        if (m.cleanContent != "0") return;
-      }
-      if (
-        m.author.bot ||
-        stop ||
-        m.channel.id !== (game_channel || process.env.botChannel)
-      )
-        return;
-
-      if (m.content.startsWith(".")) return;
-
-      if (
-        Number(m.content) === last.num + 1 &&
-        last.user !== m.channel.lastMessage.author.id
-      ) {
-        last.num += 1;
-
-        if (!m.channel.lastMessage.bot)
-          last.user = m.channel.lastMessage.author.id;
-
-        fs.writeFileSync("./db.json", JSON.stringify(last), "utf8");
-        m.react("👌");
-
-        console.log(last.user, m.channel.lastMessage.author.id);
-      } else {
-        m.reply(
-          last.user !== m.channel.lastMessage.author.id
-            ? `Yanlış sayı! (${last.num + 1})`
-            : "Son yazan kişi sensin!"
-        )
-          .then((_) => {
-            setTimeout(() => {
-              m.delete().catch(() => {
-                m.channel.send("Yeterli iznim yok!");
-              });
-              _.delete().catch(() => {
-                m.channel.send("Yeterli iznim yok!");
-              });
-            }, 3000);
+        break;
+      case "--sayi":
+        m.reply(`${last.num || "0"}`)
+          .then(() => {
+            setTimeout((_) => {
+              _?.delete();
+              m?.delete();
+            }, 3 * 1000);
           })
-          .catch(() => {
-            m.channel.send(
-              "Yeterli izinlere sahip değilim! Kullanıcının mesajını silemedim."
-            );
-          });
-      }
-      break;
+          .catch(() => {});
+        break;
+
+      case "--stop":
+        if (auth(m)) return;
+
+        stop = true;
+        m.reply("Stopping...").then((t) => {
+          setTimeout(() => {
+            t.delete().catch(() => {
+              m.channel.send("Yeterli iznim yok!");
+            });
+          }, 3000);
+        });
+        break;
+
+      case "--start":
+        if (auth(m)) return;
+
+        stop = false;
+        break;
+
+      case "--reset":
+        if (auth(m)) return;
+
+        last.num = 0;
+        last.user = "";
+        fs.writeFileSync("./db.json", JSON.stringify(last), "utf8");
+        break;
+
+      case "--gamechannel":
+        console.log(auth(m));
+        if (auth(m)) return;
+
+        game_channel = m.channel.id;
+        break;
+
+      default:
+        if (!Number(m.cleanContent)) {
+          if (m.cleanContent != "0") return;
+        }
+        if (
+          m.author.bot ||
+          stop ||
+          m.channel.id !== (game_channel || process.env.botChannel)
+        )
+          return;
+
+        if (m.content.startsWith(".")) return;
+
+        if (
+          Number(m.content) === last.num + 1 &&
+          last.user !== m.channel.lastMessage.author.id
+        ) {
+          last.num += 1;
+
+          if (!m.channel.lastMessage.bot)
+            last.user = m.channel.lastMessage.author.id;
+
+          fs.writeFileSync("./db.json", JSON.stringify(last), "utf8");
+          m.react("👌");
+
+          console.log(last.user, m.channel.lastMessage.author.id);
+        } else {
+          m.reply(
+            last.user !== m.channel.lastMessage.author.id
+              ? `Yanlış sayı! (${last.num + 1})`
+              : "Son yazan kişi sensin!"
+          )
+            .then((_) => {
+              setTimeout(() => {
+                m?.delete().catch(() => {
+                  m.channel.send("Yeterli iznim yok!");
+                });
+                _?.delete().catch(() => {
+                  m.channel.send("Yeterli iznim yok!");
+                });
+              }, 3000);
+            })
+            .catch(() => {
+              m.channel.send(
+                "Yeterli izinlere sahip değilim! Kullanıcının mesajını silemedim."
+              );
+            });
+        }
+        break;
+    }
+  } catch (err) {
+    console.error(err);
   }
 });
 
